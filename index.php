ac53bf5b0b39da6612a687828db2ab8126f2e3a1
Nahid Bin Azhar <nahd.dns@gmail.com>
2016-07-28 16:30:28 +0600

    fix configuration issue

--- index.php
+++ index.php
@@ -4,20 +4,34 @@
 *@Author URL:	http://nahid.co
 */
 
-require_once 'vendor/autoload.php';
+require_once 'src/Crudx.php';
 
-use Nahid\Crudx\Facade\Crudx;
+use Nahid\Crudx\Crudx;
+
 //use Nahid\Crudx\Crudx;
+$config = [
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => 'bolbona',
+    'database' => 'crudx_db',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => 'tbl_',
+];
 
-
-$db = new Crudx;
+$db = new Crudx($config);
 
 //Crudx::table('user')->where('id', '=', 5)->get()->result();
 
+/*
+$user = $db->table('users');
 
-$data=Crudx::table('posts')
-->first(['blog', 'id']);
+$user->name = 'Arufur Naim';
+$user->username = 'naim';
+$user->created_at = date('Y-m-d');
 
+if($user->save()) {
+    echo 'Success';
+}*/
 
-
-var_dump($data->get(['id', 'blog'])->result());
+var_dump($db->table('users')->all()->result());
